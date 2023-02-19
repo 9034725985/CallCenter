633de72d297eca327b6b2f12a1daca63e21e8fc2
Kushal <kushaldeveloper@gmail.com>
2023-02-19 10:27:55 -0600

    add a unit test using bunit
    https://learn.microsoft.com/en-us/aspnet/core/blazor/test?view=aspnetcore-7.0
    https://bunit.dev/

---
+++ ClientTests/CounterTests.cs
@@ -0,0 +1,21 @@
+using CallCenter.Client.Pages;
+
+namespace ClientTests;
+
+public class CounterTests
+{
+    [Fact]
+    public void CounterShouldIncrementWhenClicked()
+    {
+        // Arrange: render the Counter.razor component
+        using var ctx = new TestContext();
+        var cut = ctx.RenderComponent<Counter>();
+
+        // Act: find and click the <button> element to increment
+        // the counter in the <p> element
+        cut.Find("button").Click();
+
+        // Assert: first find the <p> element, then verify its content
+        cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 1</p>");
+    }
+}
