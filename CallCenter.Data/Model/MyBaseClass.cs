79647122e141eb092b5c5435929ca6022995f2bf
Kushal <kushaldeveloper@gmail.com>
2023-02-19 10:37:40 -0600

    format code
    also add a new data project for data access

---
+++ CallCenter.Data/Model/MyBaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Data.Model;
+
+public class MyBaseClass
+{
+    public int Id { get; set; }
+    public Guid ExternalId { get; set; }
+
+    public int CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int ModifiedBy { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public string CreatedByName { get; set; } = "";
+    public string ModifiedByName { get; set; } = "";
+    public Stopwatch? Stopwatch { get; set; }
+}
