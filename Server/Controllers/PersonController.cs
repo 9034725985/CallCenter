5681261ef4f41e5cd2ce5b13a13b2a5b997979a8
Kushal <kushaldeveloper@gmail.com>
2023-02-20 08:55:28 -0600

    add authorize back to get person

--- Server/Controllers/PersonController.cs
+++ Server/Controllers/PersonController.cs
@@ -3,13 +3,14 @@ using Data.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
+using System.Diagnostics;
 
 namespace CallCenter.Server.Controllers;
 
-//[Authorize]
+[Authorize]
 [ApiController]
 [Route("[controller]")]
-//[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
 public class PersonController : ControllerBase
 {
     private readonly ILogger<PersonController> _logger;
@@ -24,7 +25,13 @@ public class PersonController : ControllerBase
     [HttpGet]
     public async Task<IEnumerable<MyPerson>> Get(CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Begin {methodname} in {classname}", nameof(Get), nameof(PersonController));
+        Stopwatch stopwatch = Stopwatch.StartNew();
         List<MyPerson> persons = await _service.GetPersons(cancellationToken);
+        stopwatch.Stop();
+        _logger.LogInformation("End {methodname} in {classname}", nameof(Get), nameof(PersonController));
+        _logger.LogInformation("PerfMatters: {methodname} in {classname} returned in {stopwatchmilliseconds} milliseconds", 
+            nameof(Get), nameof(PersonController), stopwatch.ElapsedMilliseconds);
         return persons;
     }
 }
