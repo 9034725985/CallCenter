4f487e1696dc9006875a2623c3be903ef2171506
Kushal <kushaldeveloper@gmail.com>
2023-05-08 16:25:32 -0500

    add some basic benchmarking

---
+++ CallCenter.Benchmarks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using MathNet.Numerics.Statistics;
+
+int iterations = 100;
+List<double> results = new();
+string url = "https://localhost:7109/openperson";
+
+for (int i = 0; i < iterations; i++)
+{
+    using HttpClient client = new();
+    Stopwatch sw = Stopwatch.StartNew();
+    var response = await client.GetAsync(url);
+    sw.Stop();
+    long x = sw.ElapsedTicks;
+    Console.WriteLine($"Iteration {i} took {sw.ElapsedTicks} ticks");
+
+    results.Add(x);
+}
+
+double median = results.Median();
+for (int i = 0; i < results.Count; i++)
+{
+}
+Console.WriteLine($"The median time taken to get {url} out of {iterations} attempts was {median} ticks");
