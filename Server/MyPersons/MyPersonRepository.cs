ecb76731cbfec9657d17ebefa7d5ffbc23fe03ca
Kushal <kushaldeveloper@gmail.com>
2023-12-08 14:05:10 -0600

    add bulkheadPolicy

--- Server/MyPersons/MyPersonRepository.cs
+++ Server/MyPersons/MyPersonRepository.cs
@@ -5,6 +5,7 @@ using System;
 using System.Threading;
 using Polly;
 using Polly.Retry;
+using Polly.Bulkhead;
 
 namespace CallCenter.Server.MyPersons
 {
@@ -12,6 +13,7 @@ namespace CallCenter.Server.MyPersons
     {
         private readonly CallCenterDbContext _context;
         private readonly AsyncRetryPolicy _retryPolicy;
+        private readonly AsyncBulkheadPolicy _bulkheadPolicy;
 
         public MyPersonRepository(CallCenterDbContext context)
         {
@@ -19,67 +21,86 @@ namespace CallCenter.Server.MyPersons
             _retryPolicy = Policy
                 .Handle<Exception>()
                 .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            _bulkheadPolicy = Policy.BulkheadAsync(5, 10); // Limit to 5 concurrent calls with a queue of 10
         }
 
         public async Task CreateAsync(MyPerson person)
         {
-            await _retryPolicy.ExecuteAsync(async () =>
+            await _bulkheadPolicy.ExecuteAsync(async () =>
             {
-                await Task.Run(() =>
+                await _retryPolicy.ExecuteAsync(async () =>
                 {
-                    _context.Add(person);
-                    _context.SaveChanges();
+                    await Task.Run(() =>
+                    {
+                        _context.Add(person);
+                        _context.SaveChanges();
+                    });
                 });
             });
         }
 
         public async Task<MyPerson?> GetPersonAsync(int id, CancellationToken token)
         {
-            return await _retryPolicy.ExecuteAsync(async () =>
+            return await _bulkheadPolicy.ExecuteAsync(async () =>
             {
-                return await _context.Persons.Where(x => x.Id == id).FirstOrDefaultAsync<MyPerson>(token);
+                return await _retryPolicy.ExecuteAsync(async () =>
+                {
+                    return await _context.Persons.Where(x => x.Id == id).FirstOrDefaultAsync<MyPerson>(token);
+                });
             });
         }
 
         public async Task<List<MyPerson>> GetPersonsAsync(CancellationToken token)
         {
-            return await _retryPolicy.ExecuteAsync(async () =>
+            return await _bulkheadPolicy.ExecuteAsync(async () =>
             {
-                List<MyPerson> persons = await _context.Persons.ToListAsync(token);
-                return persons ?? new();
+                return await _retryPolicy.ExecuteAsync(async () =>
+                {
+                    List<MyPerson> persons = await _context.Persons.ToListAsync(token);
+                    return persons ?? new();
+                });
             });
         }
 
         public async Task<bool> GetPersonExistsAsync(int id, CancellationToken token)
         {
-            return await _retryPolicy.ExecuteAsync(async () =>
+            return await _bulkheadPolicy.ExecuteAsync(async () =>
             {
-                return await _context.Persons.AnyAsync(x => x.Id == id, token);
+                return await _retryPolicy.ExecuteAsync(async () =>
+                {
+                    return await _context.Persons.AnyAsync(x => x.Id == id, token);
+                });
             });
         }
 
         public async Task<MyInteger> PutPersonAsync(MyPerson person, CancellationToken token)
         {
-            await _retryPolicy.ExecuteAsync(async () =>
+            await _bulkheadPolicy.ExecuteAsync(async () =>
             {
-                _context.Persons.Update(person);
-                await _context.SaveChangesAsync(token);
+                await _retryPolicy.ExecuteAsync(async () =>
+                {
+                    _context.Persons.Update(person);
+                    await _context.SaveChangesAsync(token);
+                });
             });
             return new();
         }
 
         public async Task<MyInteger> PutPersonsAsync(List<MyPerson> persons, CancellationToken token)
         {
-            await _retryPolicy.ExecuteAsync(async () =>
+            await _bulkheadPolicy.ExecuteAsync(async () =>
             {
-                foreach (MyPerson person in persons)
+                await _retryPolicy.ExecuteAsync(async () =>
                 {
-                    _context.Persons.Update(person);
-                }
-                await _context.SaveChangesAsync(token);
+                    foreach (MyPerson person in persons)
+                    {
+                        _context.Persons.Update(person);
+                    }
+                    await _context.SaveChangesAsync(token);
+                });
             });
             return new();
         }
-
     }
 }
